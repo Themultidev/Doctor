16a4ca94a4195c9d01e49fa79ef87c653c24275e
Themultidev <worlup83@gmail.com>
2026-02-22 23:57:04 +0100

    update on hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,43 +4,45 @@ import {Link} from "react-router-dom";
 const Hero = ()=>{
     return(
         <>
-        <div className=" grid grid-cols-[2fr_1fr] bg-amber-300 p-4 h-screen">
-            <div className="flex flex-col  p-3 ">
+        <div className=" flex gap-8  p-4 h-screen w-full text-gray-700 bg-gray-100">
+            <div className="flex flex-col    min-w-2xl ">
                 
-                <nav className="flex justify-between items-center">
+                <nav className="flex justify-between items-center z-10">
                     <div className=""><Link to="/">Home</Link></div>
 
-                    <ul className="flex space-x-4">
-                        <li><Link to="/about">Books</Link></li>
-                        <li><Link to="/career">Career</Link></li>
-                        <li><Link to="/contact">Contact</Link></li>
-                    </ul>
+                    <div>
+                        <ul className="flex space-x-4">
+                            <li><Link to="/about">Books</Link></li>
+                            <li><Link to="/career">Career</Link></li>
+                            <li><Link to="/contact">Contact</Link></li>
+                        </ul>
+                    </div>
                 </nav>
 
-                <div className="flex items-center justify-center py-20 max-w-3xl ">
-                    <h1 className="text-9xl ">Intimacy Empower</h1>
+                <div className="flex items-center justify-center py-65  max-w-3xl ">
+                    <h1 className="text-9xl text-left font-mono  ">Intimacy Empower</h1>
                 </div>
 
-                <div></div>
-
-                
+                    
             </div>
             
-            <div className="min-w-full">
-                <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW50aW1hY3klMjBpbXBob3JlfGVufDB8fDB8fHww&w=1000&q=80" alt="hero image" className="w-full h-full object-cover"/>
+            <div className="items-center justify-end ">
+                <img 
+                src="heroimg.jpeg" 
+                alt="hero image" className="  object-cover"/>
                 
                 <div className=" flex space-x-4 max-w-3xl mx-auto">
-                    <div className="flex flex-col space-y-3 text-2xl font" >
-                        <p>Worlu Gift Azubike</p>
-                        <p>8 Years Of experience</p>
+                    <div className="flex flex-col space-y-1 leading-1" >
+                        <h2 className="text-2xl font-semibold">Worlu Gift Azubike</h2>
+                        <p className="font-light">8 Years Of experience</p>
                     </div>
 
-                    <div className="flex flex-col space-y-3" >
-                        <p>Counsultant DR</p>
-                        <div className="flex space-x-3">
-                            <p>MMRs</p>
-                            <p>PHD</p>
-                        </div>           
+                    <div className="flex flex-col space-y-1 leading-1" >
+                        <h2 className="text-2xl font-semibold">Counsultant DR</h2>
+                        <ul className="flex space-x-3 font-light *:p-4 *:rounded-full *:bg-gray-300">
+                            <li>MMRs</li>
+                            <li>PHD</li>
+                        </ul>           
                     </div>
                 </div>
             </div>
