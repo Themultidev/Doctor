eaefa7525d6b8e42e5efbc2d8d44540f9d7e149b
Themultidev <worlup83@gmail.com>
2026-03-01 14:30:51 +0100

    part of mobile display

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,14 +7,18 @@ import { ArrowUpRight } from 'lucide-react';
 const Hero = ()=>{
     return(
         <>
-        <div className="py-5">
-            <div className=" flex gap-8  p-4 h-full w-full text-gray-800 bg-gray-100">
-                <div className="flex flex-col    min-w-2xl ">
+        <div className="">
+            <div className="flex flex-col  md:flex-row md:gap-8  p-4 h-full w-full text-gray-800 bg-gray-100">
+                <div className="flex flex-col    md:min-w-2xl ">
                     
                     <Navbar/>
 
-                    <div className="flex items-center justify-center py-65  max-w-3xl ">
-                        <h1 className="text-9xl text-left font-mono  ">Intimacy Empower</h1>
+                    <div className="flex flex-col items-center justify-center py-20 md:py-65  md:max-w-3xl ">
+                        <h1 className="text-6xl text-center  md:text-9xl md:text-left font-mono  ">Intimacy Empower</h1>
+                        <div className="md:hidden">
+                            <p className="rotate-88 text-9xl  font font-thin ml-3 animate-pulse "> {">"} </p>
+                        </div>
+
                     </div>
 
                         
@@ -27,10 +31,10 @@ const Hero = ()=>{
                     
                     <div className=" flex space-x-4 max-w-3xl mx-auto ">
                         <div className="flex flex-col space-y-1 leading-1" >
-                            <h2 className="text-2xl font-semibold font-mono">Worlu Gift Azubike</h2>
+                            <h2 className="text-2xl t  font-semibold font-mono">Worlu Gift Azubike</h2>
                             <h2 className="text-2xl font-semibold font-mono">Counsultant DR</h2>
                             <p className="font-light text-sm">8 Years Of experience</p>
-                            <ul className="flex space-x-3 font- font-mono *:p-4 *:rounded *:bg-gray-900 text-amber-50">
+                            <ul className="flex  space-x-3 font- font-mono *:p-4 *:rounded *:bg-gray-900 text-amber-50">
                                 <li>MMRs</li>
                                 <li>PHD</li>
                             </ul>   
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,69 @@
 import {Link} from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = ()=>{
+    
+    const [isOpen, setisOpen] = useState(false);
+    
+    const handleisOpen = ()=>{
+        setisOpen(!isOpen)
+    }
+
     return(
         <>
+            {/* <div className="block md:hidden">
+                <nav className="flex justify-between items-center z-10 text-lg font-thin ">
 
-            <nav className="flex justify-between items-center z-10 text-lg font-thin">
-
-                <div className=""><Link to="/">Home</Link></div>
+                    <div className=""><Link to="/">Home</Link></div>
                     <div>
+
+                        <button onClick={handleisOpen}>svg</button>
+
+                        <div className={isOpen? "block":"hidden"}>
+                            <ul className=" flex flex-col space-x-4">
+                                <li><Link to="/about">Books</Link></li>
+                                <li><Link to="/career">Career</Link></li>
+                                <li><Link to="/contact">Contact</Link></li>
+                            </ul>
+                        </div>
+
+                        
+                    </div>
+                </nav>
+            </div> */}
+
+            
+            <nav className=" z-10 text-lg font-thin border-b md:border-b-0 py-2 md:py-0   ">
+                <div className="flex justify-between items-center">
+                    <div className=""><Link to="/">Home</Link></div>
+                    
+                    <div className=" block md:hidden">
+                        <button onClick={handleisOpen}>svg</button>
+                    </div>
+                    
+                    <div className="hidden md:block">
                         <ul className="flex space-x-4">
                             <li><Link to="/about">Books</Link></li>
                             <li><Link to="/career">Career</Link></li>
                             <li><Link to="/contact">Contact</Link></li>
                         </ul>
                     </div>
+                    
+                </div>
+
+                <div className="block  md:hidden absolute mt-5 left-0 right-0">
+                    <div className={`${isOpen ? 'block' : 'hidden'} bg-amber-300 `}>
+                        <ul className="flex flex-col items space-y-4 ">
+                            <li><Link to="/about">Books</Link></li>
+                            <li><Link to="/career">Career</Link></li>
+                            <li><Link to="/contact">Contact</Link></li>
+                        </ul>
+                    </div>
+
+                </div>
+
             </nav>
+            
         
         </>
     )
